0e20522549bdcba0a621960e47e428975396193b
Harry Hopkinson <harryhopkinson@Harrys-MacBook-Air.local>
2024-01-02 21:19:40 +0000

    Update script to fix errors

--- init.lua
+++ init.lua
@@ -30,27 +30,6 @@ require('packer').startup(function(use)
   -- set termguicolors to enable highlight groups
   vim.opt.termguicolors = true
 
-  -- empty setup using defaults
-  require("nvim-tree").setup()
-  -- OR setup with some options
-  require("nvim-tree").setup({
-    sort_by = "case_sensitive",
-    view = {
-    adaptive_size = true,
-    mappings = {
-      list = {
-        { key = "u", action = "dir_up" },
-      },
-    },
-  },
-  renderer = {
-    group_empty = true,
-  },
-  filters = {
-    dotfiles = true,
-  },
-})
-
   use { -- Autocompletion
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
@@ -77,19 +56,11 @@ require('packer').startup(function(use)
   use 'lewis6991/gitsigns.nvim'
 
   -- Tabs for neovim
-  use 'nvim-tree/nvim-web-devicons'
-  use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
+  -- use {'romgrk/barbar.nvim', wants = 'nvim-web-devicons'}
 
   use 'morhetz/gruvbox' -- Theme inspired by Gruvbox
   use 'nvim-lualine/lualine.nvim' -- Fancier statusline
   use 'lukas-reineke/indent-blankline.nvim' -- Add indentation guides even on blank lines
-  use {
-    'nvim-tree/nvim-tree.lua',
-    requires = {
-      'nvim-tree/nvim-web-devicons', -- optional, for file icons
-    },
-    tag = 'nightly' -- optional, updated every week. (see issue #1193)
-  }
   use 'numToStr/Comment.nvim' -- "gc" to comment visual regions/lines
   use 'tpope/vim-sleuth' -- Detect tabstop and shiftwidth automatically
 
@@ -268,11 +239,9 @@ require('Comment').setup()
 
 -- Enable `lukas-reineke/indent-blankline.nvim`
 -- See `:help indent_blankline.txt`
-require('indent_blankline').setup {
-  char = '┊',
-  show_trailing_blankline_indent = false,
+require('ibl').setup {
+  indent = { char = '┊' },
 }
-
 -- Gitsigns
 -- See `:help gitsigns.txt`
 require('gitsigns').setup {
@@ -323,7 +292,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'lua', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'lua' },
 
   highlight = { enable = true },
   indent = { enable = true },
@@ -442,7 +411,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'tsserver', 'sumneko_lua'  }
+local servers = { 'clangd', 'tsserver'  }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
@@ -470,7 +439,7 @@ local runtime_path = vim.split(package.path, ';')
 table.insert(runtime_path, 'lua/?.lua')
 table.insert(runtime_path, 'lua/?/init.lua')
 
-require('lspconfig').sumneko_lua.setup {
+require('lspconfig').lua_ls.setup {
   on_attach = on_attach,
   capabilities = capabilities,
   settings = {
