72d47049cf7e045bc7cd2794f28de6804ff16b3f
Harry-Hopkinson <harryjhopkinson@gmail.com>
2024-01-05 22:19:17 +0000

    Update starter language servers

--- init.lua
+++ init.lua
@@ -413,7 +413,7 @@ require('mason').setup()
 
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
-local servers = { 'clangd', 'tsserver'  }
+local servers = { 'lua_ls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
