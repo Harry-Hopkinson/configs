bcf4e65cc36573e5080c8b7f1d4e7e08268c345e
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2024-01-20 20:46:24 +0000

    Add files via upload

--- init.lua
+++ init.lua
@@ -383,23 +383,22 @@ require('mason').setup()
 -- Enable the following language servers
 -- Feel free to add/remove any LSPs that you want here. They will automatically be installed
 local servers = { 'clangd' }
--- local servers = { 'lua_ls' }
 
 -- Ensure the servers above are installed
 require('mason-lspconfig').setup {
-  ensure_installed = servers,
+  -- ensure_installed = servers,
 }
 
 -- nvim-cmp supports additional completion capabilities
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities = require('cmp_nvim_lsp').default_capabilities(capabilities)
 
-for _, lsp in ipairs(servers) do
-  require('lspconfig')[lsp].setup {
-    on_attach = on_attach,
-    capabilities = capabilities,
-  }
-end
+-- for _, lsp in ipairs(servers) do
+  -- require('lspconfig')[lsp].setup {
+    -- on_attach = on_attach,
+    -- capabilities = capabilities,
+  -- }
+-- end
 
 -- Turn on lsp status information
 -- require('fidget').setup()
