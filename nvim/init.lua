f43048f94b712dcaa4e76eb9f0ee69fb604363fc
Harry Hopkinson <63599884+Harry-Hopkinson@users.noreply.github.com>
2024-07-03 15:58:21 +0100

    Change gruvbox theme

--- nvim/init.lua
+++ nvim/init.lua
@@ -51,8 +51,8 @@ local plugins = {
  -- Autoclose Brackets
  'm4xshen/autoclose.nvim',
 
-  { 
-    'morhetz/gruvbox', -- Theme inspired by Gruvbox
+  {
+    'ellisonleao/gruvbox.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
